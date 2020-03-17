5052898048df2ad8f1c54f8d78eb2505e5bcf87a
florent <fcapocci@student.42.fr>
2020-03-17 01:41:21 +0100

    feat(linter): add jest test in eslintrc

--- __tests__/document.test.js
+++ __tests__/document.test.js
@@ -1,34 +1,34 @@
-// const request = require('supertest');
-// const app = require ('../app');
+const request = require('supertest');
+const app = require ('../app');
 
-// describe('All CRUD enpoints', () => {
-//   /* document creation */
+describe('All CRUD enpoints', () => {
+  /* document creation */
 
-//   it('should create a new document', async () => {
-//     const res = await request(app)
-//       .post('/')
-//       .send({
-//           document: '{"property1":"value1","property2":"value2","property3":"value3", "property4": { "property1.1": ["value1.1.1", 2, "value1.1.2"] }}',
-//       });
+  it('should create a new document', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({
+          "document": '{"property1":"value1","property2":"value2","property3":"value3", "property4": { "property1.1": ["value1.1.1", 2, "value1.1.2"] }}',
+      });
 
-//     expect(res.statusCode).toEqual(201);
-//   });
+    expect(res.statusCode).toEqual(201);
+  });
 
-//   it('should fail creating a new document', async () => {
-//     const res = await request(app)
-//       .post('/')
-//       .send({
-//         toto: 'pouet',
-//         num: 12,
-//       });
+  it('should fail creating a new document', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({
+        "property1": 'value1',
+        "property2": "value2",
+      });
 
-//     expect(res.statusCode).toEqual(400);
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         code: 'validation',
-//         httpCode: 400,
-//         details: ['"document" is required'],
-//       }),
-//     );
-//   });
-// });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        code: 'validation',
+        httpCode: 400,
+        details: ['"document" is required'],
+      }),
+    );
+  });
+});
